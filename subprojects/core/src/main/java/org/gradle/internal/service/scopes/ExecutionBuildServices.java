7024852defbb86a12b7409b5bafbf1182572d3ec
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-04-25 21:07:33 -0300

    Fix Rust substrate after upstream rebase

--- subprojects/core/src/main/java/org/gradle/internal/service/scopes/ExecutionBuildServices.java
+++ subprojects/core/src/main/java/org/gradle/internal/service/scopes/ExecutionBuildServices.java
@@ -18,7 +18,7 @@ package org.gradle.internal.service.scopes;
 import org.gradle.StartParameter;
 import org.gradle.api.internal.cache.StringInterner;
 import org.gradle.api.internal.changedetection.state.DefaultExecutionHistoryCacheAccess;
-import org.gradle.api.problems.internal.InternalProblems;
+import org.gradle.api.problems.internal.ProblemsInternal;
 import org.gradle.cache.FileLockManager;
 import org.gradle.cache.PersistentCache;
 import org.gradle.cache.internal.InMemoryCacheDecoratorFactory;
@@ -184,7 +184,7 @@ public class ExecutionBuildServices implements ServiceRegistrationProvider {
         OverlappingOutputDetector overlappingOutputDetector,
         StartParameter startParameter,
         TimeoutHandler timeoutHandler,
-        InternalProblems problems,
+        ProblemsInternal problems,
         WorkerLeaseService workerLeaseService,
         InternalOptions options,
         @Nullable SubstrateClient substrateClient,
