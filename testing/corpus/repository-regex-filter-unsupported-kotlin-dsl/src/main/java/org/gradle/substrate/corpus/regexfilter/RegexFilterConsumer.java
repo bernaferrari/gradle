8a83523cf0c893984c19771b337e3dd480f90671
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-05-11 17:51:43 -0300

    Fail closed repository regex content filters

---
+++ testing/corpus/repository-regex-filter-unsupported-kotlin-dsl/src/main/java/org/gradle/substrate/corpus/regexfilter/RegexFilterConsumer.java
@@ -0,0 +1,7 @@
+package org.gradle.substrate.corpus.regexfilter;
+
+public final class RegexFilterConsumer {
+    public String message() {
+        return "repository regex filter remains fail-closed";
+    }
+}
