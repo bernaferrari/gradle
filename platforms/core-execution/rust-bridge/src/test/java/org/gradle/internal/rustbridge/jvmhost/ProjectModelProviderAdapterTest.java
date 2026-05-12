7be67a5e70f7691179751998e9ee2223de1fea51
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-05-12 06:03:10 -0300

    Promote exact component metadata status rule

--- platforms/core-execution/rust-bridge/src/test/java/org/gradle/internal/rustbridge/jvmhost/ProjectModelProviderAdapterTest.java
+++ platforms/core-execution/rust-bridge/src/test/java/org/gradle/internal/rustbridge/jvmhost/ProjectModelProviderAdapterTest.java
@@ -729,10 +729,36 @@ public class ProjectModelProviderAdapterTest {
     }
 
     @org.junit.Test
-    public void marksComponentMetadataRulesAsUnsupportedDependencySemantics() throws IOException {
+    public void exactReleaseComponentMetadataStatusRuleIsNativeReady() throws IOException {
         File buildFile = temporaryFolder.newFile("build.gradle.kts");
         Files.write(buildFile.toPath(), Collections.singletonList(
             "dependencies { components { all { status = \"release\" } } }"
+        ), StandardCharsets.UTF_8);
+        Task task = basicFileTransformTask(
+            ":classes",
+            "classes",
+            fileCollection(),
+            fileCollection(),
+            Collections.emptyMap(),
+            false,
+            false,
+            buildFile
+        );
+
+        BuildPlanTask planTask = ProjectModelProviderAdapter.toBuildPlanTask(task, DefaultTask.class);
+        Map<String, String> inputs = planTask.getInputSpecsList().stream()
+            .filter(input -> input.getKind().equals("value"))
+            .collect(Collectors.toMap(BuildPlanTaskInputSpec::getName, BuildPlanTaskInputSpec::getValue));
+
+        assertFalse(inputs.containsKey("unsupported_dependency_semantics"));
+        assertFalse(inputs.getOrDefault("unsupported_repository_features", "").contains("component-metadata-rule"));
+    }
+
+    @org.junit.Test
+    public void nontrivialComponentMetadataRuleRemainsUnsupportedDependencySemantics() throws IOException {
+        File buildFile = temporaryFolder.newFile("build.gradle.kts");
+        Files.write(buildFile.toPath(), Collections.singletonList(
+            "dependencies { components { all { status = \"integration\" } } }"
         ), StandardCharsets.UTF_8);
         Task task = basicFileTransformTask(
             ":classes",
